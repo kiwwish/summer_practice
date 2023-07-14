4f6c396d1084db1f7750e11bdbb39a4fa8f8ad1a
Kuznetsova Varvara <XvaryaX@yandex.ru>
2023-07-15 01:51:08 +0700

    Commit 7: change task 4 of block 4

--- Pt_4/Pt_4_4.py
+++ Pt_4/Pt_4_4.py
@@ -1,10 +1,9 @@
 import itertools
-
-nums = list(map(int, input("Введите числа: ").split()))
-value = int(input("Введите число: "))
-ans = []
-for i in range(1, len(nums) + 1):
-    for el in itertools.combinations(nums, i):
-        if sum(el) == value:
-            ans.append(sorted(el))
-print(ans)
+list = list(map(int, input("Введите числа: ").split()))
+a = int(input("Введите число: "))
+new_list = []
+for i in range(1, len(list) + 1):
+    for el in itertools.combinations(list, i):
+        if sum(el) == a:
+            new_list.append(sorted(el))
+print(new_list)
