9104f9ee825212053e04c930409b5a8b55fae75b
Kuznetsova Varvara <XvaryaX@yandex.ru>
2023-07-21 14:28:47 +0700

    Commit 10: changing tasks using pycodestyle

--- Pt_1/Pt_1_2.py
+++ Pt_1/Pt_1_2.py
@@ -1,11 +1,4 @@
 name = str(input("Введите имя: "))
-<<<<<<< HEAD
 surname = str(input("Введите фамилию: "))
 full_name = name + " " + surname
-print("Ваше полное имя и его длина(с учётом пробела): ", full_name,
-      ", ", len(full_name))
-=======
-surname = str (input("Введите фамилию: "))
-full_name = name+" "+surname
-print ("Ваше полное имя и его длина(с учётом пробела): ",full_name,", ", len(full_name))
->>>>>>> d55fe4008be2a103bf49a2111395892436f94b6e
+print("Ваше полное имя и его длина(с учётом пробела): ", full_name, ", ", len(full_name))
--- Pt_1/Pt_1_3.py
+++ Pt_1/Pt_1_3.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 a = int(input("Введите первое число: "))
 b = int(input("Введите второе число: "))
 c = int(input("Введите третье число: "))
@@ -20,26 +19,4 @@ else:
         print(c, b, a)
     else:
         print(c, a, b)
-=======
-a = int (input("Введите первое число: "))
-b = int (input("Введите второе число: "))
-c = int (input("Введите третье число: "))
-if a > b and a > c:
-    print ("Наибольшее число: ", a)
-    if b > c:
-        print (a,b,c)
-    else:
-        print(a,c,b)
-elif b > c and b > a:
-    print("Наибольшее число: ", b)
-    if c > a:
-        print (b,c,a)
-    else:
-        print (b,a,c)
-else:
-    print("Наибольшее число: ", c)
-    if b > a:
-        print (c,b,a)
-    else:
-        print (c,a,b)
->>>>>>> d55fe4008be2a103bf49a2111395892436f94b6e
+
